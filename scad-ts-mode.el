0cf2aed540b9e380b3724247daf8917ed3563e57
Yoshinari Nomura <nom@quickhack.net>
2026-01-03 16:23:39 +0900

    Add better commentary

--- scad-ts-mode.el
+++ scad-ts-mode.el
@@ -27,8 +27,12 @@
 
 ;;; Commentary:
 ;;
-;;  This program requires ~/.emacs.d/tree-sitter/libtree-sitter-openscad.so
-;;  See: https://github.com/openscad/tree-sitter-openscad/
+;; A major mode for OpenSCAD with Tree-sitter support for indentation,
+;; font-locking, and imenu integration.
+;;
+;; This program requires ~/.emacs.d/tree-sitter/libtree-sitter-openscad.so
+;; After install this package, run M-x scad-ts-mode-install-language-grammar.
+;;
 
 ;;; Code:
 
